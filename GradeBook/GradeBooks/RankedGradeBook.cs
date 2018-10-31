bd0a1ce4eb9b70b4885d8a70afc10e35737e446d
Luke Bilsborrow <fakerstuna@gmail.com>
2018-10-31 14:28:21 +0000

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,23 +21,12 @@ namespace GradeBook.GradeBooks
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-            List<char> outputGrades = new List<char> { 'A', 'B', 'C', 'D' };
 
-            for (int i=0;i<6;i++)
-            {
-                double grade = grades[i];
-                if (grade > averageGrade) continue;
-                else
-                {
-                    if (grade == 5) return '5';
-                    return outputGrades[i];
-                }
-            }
+            if (grades[threshold - 1] <= averageGrade) return 'A';
+            else if (grades[threshold * 2 - 1] <= averageGrade) return 'B';
+            else if (grades[threshold * 3 - 1] <= averageGrade) return 'C';
+            else if (grades[threshold * 4 - 1] <= averageGrade) return 'D';
             return 'F';
-            
-            
-
-            
         }
     }
 }
